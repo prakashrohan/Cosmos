230e968fb80a92c1dd3ef0e042ba447107fc9150
Rohan Prakash <pksh.rohan16@gmail.com>
2025-01-17 10:26:05 +0530

    [UPDATE] Launch Screen

---
+++ Cosmos/Extensions/color.swift
@@ -0,0 +1,34 @@
+//
+//  color.swift
+//  Cosmos
+//
+//  Created by Rohan Prakash on 17/01/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme{
+    
+    let accent = Color("0CD5DC")
+    let main = Color("7447F7")
+    let backgroundcolor = Color("091222")
+    let backgroundcolorTwo = Color("081222")
+    let black = Color("232832")
+    let main2 = Color("AC91FA")
+    let black2 = Color("777D87")
+    let red = Color("E01A26")
+    let darkPurple = Color("6E40D7")
+    let lightPurple = Color("B985FF")
+    let darkGray=Color("2C2C2C")
+    let darkGreen=Color("389D5F")
+    let purplemain=Color("5D06D2")
+    
+}
+
---
+++ Cosmos/Views/Home/homeView.swift
@@ -0,0 +1,22 @@
+//
+//  homeView.swift
+//  Cosmos
+//
+//  Created by Rohan Prakash on 16/01/25.
+//
+
+import SwiftUI
+
+struct homeView: View {
+    var body: some View {
+        
+        
+        NavigationStack {
+            Text("Hello World!")
+        }
+    }
+}
+
+#Preview {
+    homeView()
+}
