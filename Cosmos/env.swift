3efd70ebedc4888ed89df7d6cbac9d9df86b08bf
Rohan Prakash <pksh.rohan16@gmail.com>
2025-07-16 12:20:17 +0530

    [Fix] skyMapService

---
+++ Cosmos/env.swift
@@ -0,0 +1,14 @@
+//
+//  env.swift
+//  Cosmos
+//
+//  Created by Rohan Prakash on 16/07/25.
+//
+
+import Foundation
+
+class Environment {
+    static let skymapApplicationID = "d78a61a2-3933-4cde-ae5a-88b6b3cdf088"
+    static let skymapApplicationSecreet = "c59e5e1b75005e1aed74c11bdd509b96b17c2e30998221076dda40b4ca848e71c616d8d3e7ced565b27dab43eb7eaac28685dbe4a5dedec17a00a9929cf2f97147df8af03b99b1b540f985baa80e862819d032ba8af2ba06a7e0504722f356efabf122082eb3df0843e34efa7e139263" 
+}
+
